387221619ad9a8d45bdd2841bcc3a30f1176619c
Ben David <66976120+bmdavid2@users.noreply.github.com>
2024-10-23 12:13:45 -0400

    fix show method for well

--- src/physical_types/Well.jl
+++ src/physical_types/Well.jl
@@ -8,13 +8,13 @@ end
 
 
 function Base.show(io::IO,w::Well)
-    print(io,"Well ID: $(w.id)",",","Labware ID: ",w.labwareid," (",w.container,")",", Well ",w.wellindex)
+    print(io,"Well ID: $(w.id)",",","Labware ID: ",w.locationid," (",w.container,")",", Well ",w.wellindex)
 end 
 
 
 function Base.show(io::IO, ::MIME"text/plain", w::Well)
     println(io, "Well ID: ",w.id)
-    println(io,"Labware ID: ",w.labwareid," (",w.container,")")
+    println(io,"Labware ID: ",w.locationid," (",w.container,")")
     println(io,"Well ",w.wellindex)
 end 
 #= 
