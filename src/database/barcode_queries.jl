0272b127bff8ded47a44b611ba0a8d8fc0d8ed7d
Ben David <66976120+bmdavid2@users.noreply.github.com>
2025-03-18 15:33:24 -0400

    added get_all_barcodes function

--- src/database/barcode_queries.jl
+++ src/database/barcode_queries.jl
@@ -11,3 +11,15 @@ function get_barcode(barcode::String)
     return bc 
 end 
 
+function get_all_barcodes(location_id::Integer;return_limit::Integer=3)
+
+    x="""SELECT Barcode,Name FROM Barcodes WHERE LocationID = $location_id LIMIT $return_limit"""
+    
+    out=query_db(x)
+    bcs = Barcode[] 
+    for row in eachrow(out) 
+        bc = Barcode(UUIDs.UUID(row.Barcode),row.Name,location_id)
+        push!(bcs,bc)
+    end 
+    return bcs 
+end 
